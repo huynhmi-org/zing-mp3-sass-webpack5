77200b93f140ab934a3242f120eb99aaf9a7fdd8
Ha Gonin <97175351+hagonin@users.noreply.github.com>
2022-10-07 22:43:01 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import '../node_modules/@fortawesome/fontawesome-free/js/all';
 import '../node_modules/@fortawesome/fontawesome-free/scss/fontawesome.scss';
 import '../node_modules/chart.js/dist/chart';
 import './js/carousel.js';
-import './js/zingchart.js';
 import './js/main.js';
 import user_default from './assets/images/user_default.jpg'
 
