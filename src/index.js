cb06b1b38386aae65eb65da93f61cf3c6bceeaf7
Pham Mi <huynhmi1209@gmail.com>
2022-10-24 15:43:26 +0700

    add button more on mobile

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import './js/zingchart.js';
 import './js/slider.js';
 import './js/main.js';
 import './js/player.js';
-import user_default from './assets/images/user_default.jpg'
+import user_default from './assets/images/user_default.jpg';
+import vip from './assets/images/vip.png';
 
 const user_icon = document.getElementById('user_icon');
 user_icon.src = user_default;
+
